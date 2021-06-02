93a52969b1e2effaf5ed1341fedba74439dfa055
Mikael Ukko Olavi Ryynänen <s9rymi00@students.osao.fi>
2021-06-02 13:49:19 +0300

    test

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,17 @@
 
+<?php
+session_start();
+$_SESSION["favcolor"] = "green";
+?>
 <html>
 	<link rel="stylesheet" href="styleshit.css">
 	<head>
 		</h1>hello world</h1>
-		<form action="vara.php" method="post">
 		<div class="box-wrapper">
-		<div id="box1" onclick="value = "23" "> test</div>
+		<div id="box1" onclick="<?php $_SESSION["favcolor"] = "gree"; ?> "> test</div>
 		</div> 
-		<input type="submit">
-		</form>
+		< <form action="vara.php" method="get">
+        <input type="submit" value="Run me now!">
+    </form>
 	</head>
 </html>
