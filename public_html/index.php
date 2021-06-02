63e7b03190aa434f58239da3c9e02c094bec3ac3
Mikael Ukko Olavi Ryynänen <s9rymi00@students.osao.fi>
2021-06-02 13:32:58 +0300

    f

--- public_html/index.php
+++ public_html/index.php
@@ -3,9 +3,9 @@
 	<link rel="stylesheet" href="styleshit.css">
 	<head>
 		</h1>hello world</h1>
-		<form action="welcome.php" method="post">
+		<form action="vara.php" method="post">
 		<div class="box-wrapper">
-		<div id="box1">test</div>
+		<div id="box1" onclick="<input type="hidden" id="custId" name="custId" value="3487">"> test</div>
 		</div> 
 		<input type="submit">
 		</form>
