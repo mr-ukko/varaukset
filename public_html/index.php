12d22342f4c5a9e24820b9402ff32b36e70024b1
Mikael Ukko Olavi Ryynänen <s9rymi00@students.osao.fi>
2021-06-02 13:35:06 +0300

    f

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,7 @@
 		</h1>hello world</h1>
 		<form action="vara.php" method="post">
 		<div class="box-wrapper">
-		<div id="box1" onclick="<input type="hidden" id="custId" name="custId" value="3487">"> test</div>
+		<div id="box1" onclick="value = "23" "> test</div>
 		</div> 
 		<input type="submit">
 		</form>
