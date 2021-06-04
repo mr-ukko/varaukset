d2ff953e1a9a6b26383b5a758d2f49a2fe18ffbd
Mikael Ukko Olavi Ryynänen <s9rymi00@students.osao.fi>
2021-06-04 11:34:28 +0300

    test

--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,13 @@
+<!DOCTYPE html>
 <html>
 	<link rel="stylesheet" href="styleshit.css">
 	<head>
 		<h1>hello world</h1>
 		<div class="box-wrapper">
-		<div id="box1" > <!--<?php $conn = mysqli_connect('localhost','root','root','varaukset'); $query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; $result = mysqli_query($conn,$query); if ($result==1) {echo 'style="background-color: coral;"';} if ($result==2) {echo 'background-color: red;"'} ?> --> test</div>
+		<div id="box1" > <!-- <?php $conn = mysqli_connect('localhost','root','root','varaukset'); $query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; $result = mysqli_query($conn,$query); if ($result==1) {echo 'style="background-color: coral;"';} if ($result==2) {echo 'background-color: red;"'} ?> --> test</div>
 		</div> 
 		<form action="vara.php/?@=tettapaikka1" method="get">
-        <input type="submit" value="Run me now!">
+        <input type="submit" value="Run me now!"></input>
     </form>
 	</head>
 </html>
