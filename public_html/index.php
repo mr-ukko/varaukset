b1f605b7a6b87ef0be357eb6db820b86b761ec18
Mikael Ukko Olavi Ryynänen <s9rymi00@students.osao.fi>
2021-06-04 11:23:33 +0300

    ooppsie

--- public_html/index.php
+++ public_html/index.php
@@ -3,7 +3,7 @@
 	<head>
 		</h1>hello world</h1>
 		<div class="box-wrapper">
-		<div id="box1" <?php $conn = mysqli_connect('localhost','root','root','varaukset'); $query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; $result = mysqli_query($conn,$query); if ($result=="1") {background-color: coral;} if ($result=="2") {background-color: red;} ?> > test</div>
+		<div id="box1" <?php $conn = mysqli_connect('localhost','root','root','varaukset'); $query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; $result = mysqli_query($conn,$query); if ($result=="1") {echo 'style="background-color: coral;"';} if ($result=="2") {echo 'background-color: red;"'} ?> > test</div>
 		</div> 
 		<form action="vara.php/?@=tettapaikka1" method="get">
         <input type="submit" value="Run me now!">
