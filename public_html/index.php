22b5fd075107f1a593bdcca0e83dc0c72b392ba0
Mikael Ukko Olavi Ryynänen <s9rymi00@students.osao.fi>
2021-06-04 11:15:24 +0300

    test for change

--- public_html/index.php
+++ public_html/index.php
@@ -1,16 +1,11 @@
-
-<?php
-session_start();
-$_SESSION["favcolor"] = "red";
-?>
 <html>
 	<link rel="stylesheet" href="styleshit.css">
 	<head>
 		</h1>hello world</h1>
 		<div class="box-wrapper">
-		<div id="box1" onclick="<?php $_SESSION["favcolor"] = "green"; ?> "  style="background-color: <?php $_SESSION["favcolor"] ?>;" > test</div>
+		<div id="box1" <?php $conn = mysqli_connect('localhost','root','root','varaukset'); $query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; $result = mysqli_query($conn,$query); if ($result=="1") {background-color: coral;} if ($result=="2") {background-color: red;} ?> > test</div>
 		</div> 
-		< <form action="vara.php" method="get">
+		<form action="vara.php/?@=tettapaikka1" method="get">
         <input type="submit" value="Run me now!">
     </form>
 	</head>
--- public_html/vara.php
+++ public_html/vara.php
@@ -1,8 +1,35 @@
-<html>
-<body>
-
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["value"]; ?>
-
-</body>
-</html> 
+<?php
+ $conn = mysqli_connect('localhost','root','root','varaukset');
+    if (!$conn){
+    echo "<h2>Ei yhteyttä tietokantaan</h2>";
+    }
+    else {
+        /*switch ($_GET['@']) {
+            case "Class":
+                $query ="SELECT * FROM alltunes ORDER BY ID ASC";
+                echo "Class";
+            break;
+            case "Tuner":
+                $query ="SELECT * FROM alltunes ORDER BY Tuner ASC";
+                echo "Tuunaaja";
+            break;
+            case "Car_year":
+                $query ="SELECT * FROM alltunes ORDER BY Year ASC";
+                echo "Car_year";
+            break;
+            case "Car_name":
+                $query ="SELECT * FROM alltunes ORDER BY ManufacturerCar_name ASC";
+                echo "Car_name";
+            break;
+            case "Tarkoitus":
+                $query ="SELECT * FROM alltunes ORDER BY purpose ASC";
+                echo "Tarkoitus";
+            break;
+            default:
+            break;
+        }*/
+        $query = "UPDATE nyt SET varaus = '2' WHERE nimi = '" + $_GET['@']+"'";
+    }
+    $result_insert = mysqli_query($conn,$query);
+    header("Refresh:0; url=index.php");
+?>
