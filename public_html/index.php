ed82f138b7e045aa4b7b7e9dfdb3ce584e91d3dd
Mikael Ukko Olavi Ryynänen <s9rymi00@students.osao.fi>
2021-06-04 11:55:38 +0300

    ff

--- public_html/index.php
+++ public_html/index.php
@@ -4,10 +4,11 @@
 	<head>
 		<h1>hello world</h1>
 		<div class="box-wrapper">
-		<div id="box1" style="<?php $conn = mysqli_connect('localhost','root','root','varaukset'); $query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; $result = mysqli_query($conn,$query); if ($result==1) {echo 'background-color: coral;';} if ($result==2) {echo 'background-color: red;';} ?>"> <?php $conn = mysqli_connect('localhost','root','root','varaukset'); $query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; $result = mysqli_query($conn,$query); echo "" + $result; ?> test</div>
+		<div id="box1" style="<?php $conn = mysqli_connect('localhost','root','root','varaukset'); $query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; $result = mysqli_query($conn,$query); if ($result==1) {echo 'background-color: coral;';} if ($result==2) {echo 'background-color: red;';} ?>"> test</div>
 		</div> 
 		<form action="vara.php/?@=tettapaikka1" method="get">
         <input type="submit" value="Run me now!"></input>
     </form>
+	<?php $conn = mysqli_connect('localhost','root','root','varaukset'); $query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; $result = mysqli_query($conn,$query); echo "" + $result; ?>
 	</head>
 </html>
