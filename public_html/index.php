11f2e013348a2824e3346bfb460576e4ec7798de
Mikael Ukko Olavi Ryynänen <s9rymi00@students.osao.fi>
2021-06-09 10:52:44 +0300

    f

--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
+<?php $conn = mysqli_connect('localhost','root','root','varaukset'); 
+$query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; 
+$result = mysqli_query($conn,$query); 
+$row = mysqli_fetch_array($result); 
+ ?>
 <html>
 	<link rel="stylesheet" href="styleshit.css">
 	<head>
 		<h1>hello world</h1>
 		<div class="box-wrapper">
-		<div id="box1" style="<?php $conn = mysqli_connect('localhost','root','root','varaukset'); $query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; $result = mysqli_query($conn,$query); $row = mysqli_fetch_array($result); if ($row["varaus"] == 1) {echo 'background-color: blue;';} if ($row["varaus"] == 2) {echo 'background-color: red;';} ?>"> test</div>
+		<div id="box1" style="<?php if ($row["varaus"] == 1) {echo 'background-color: blue;';} if ($row["varaus"] == 2) {echo 'background-color: red;';} ?>"> test</div>
 		</div> 
 		<form action="vara.php/?@=tettapaikka1" method="get">
         <input type="submit" value="Run me now!"></input>
