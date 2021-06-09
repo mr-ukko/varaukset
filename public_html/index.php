09644cc25a2868feb1c853b84dd4fdce4d7e423a
Mikael Ukko Olavi Ryynänen <s9rymi00@students.osao.fi>
2021-06-09 13:51:05 +0300

    gg

--- public_html/index.php
+++ public_html/index.php
@@ -15,7 +15,11 @@ $row = mysqli_fetch_array($result);
         <input type="submit" value="Run me now!"></input>
     </form>
 	<?php 
-	
+	$query=SELECT * FROM `nyt` ;
+	$result = mysqli_query($conn,$query);
+        while ($row = mysqli_fetch_array($result)) {
+        echo "<option value='"+$row["nimi"] +">"+ $row["nimi"]+>"</option>";}
+	 <option value="volvo">Volvo</option>
 	?>
 	</head>
 </html>
