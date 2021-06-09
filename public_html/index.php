848e76a41bfcfc5660c0f1703cb91bb75ebe96d7
Mikael Ukko Olavi Ryynänen <s9rymi00@students.osao.fi>
2021-06-09 12:52:29 +0300

    f

--- public_html/index.php
+++ public_html/index.php
@@ -11,9 +11,11 @@ $row = mysqli_fetch_array($result);
 		<div class="box-wrapper">
 		<div id="box1" style="<?php if ($row["varaus"] == 1) {echo 'background-color: blue;';} if ($row["varaus"] == 2) {echo 'background-color: red;';} ?>"> test</div>
 		</div> 
-		<form action="vara.php/?@=tettapaikka1" method="get">
+		<form action="vara.php" method="get">
         <input type="submit" value="Run me now!"></input>
     </form>
-	<?php $conn = mysqli_connect('localhost','root','root','varaukset'); $query="SELECT varaus FROM nyt WHERE nimi='tettapaikka1'"; $result = mysqli_query($conn,$query); echo "" + $result; ?>
+	<?php 
+	
+	?>
 	</head>
 </html>
--- public_html/vara.php
+++ public_html/vara.php
@@ -1,35 +1,3 @@
 <?php
- $conn = mysqli_connect('localhost','root','root','varaukset');
-    if (!$conn){
-    echo "<h2>Ei yhteyttä tietokantaan</h2>";
-    }
-    else {
-        /*switch ($_GET['@']) {
-            case "Class":
-                $query ="SELECT * FROM alltunes ORDER BY ID ASC";
-                echo "Class";
-            break;
-            case "Tuner":
-                $query ="SELECT * FROM alltunes ORDER BY Tuner ASC";
-                echo "Tuunaaja";
-            break;
-            case "Car_year":
-                $query ="SELECT * FROM alltunes ORDER BY Year ASC";
-                echo "Car_year";
-            break;
-            case "Car_name":
-                $query ="SELECT * FROM alltunes ORDER BY ManufacturerCar_name ASC";
-                echo "Car_name";
-            break;
-            case "Tarkoitus":
-                $query ="SELECT * FROM alltunes ORDER BY purpose ASC";
-                echo "Tarkoitus";
-            break;
-            default:
-            break;
-        }*/
-        $query = "UPDATE nyt SET varaus = '2' WHERE nimi = '" + $_GET['@']+"'";
-    }
-    $result_insert = mysqli_query($conn,$query);
     header("Refresh:0; url=index.php");
 ?>
