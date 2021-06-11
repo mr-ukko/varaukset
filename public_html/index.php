61200274eed32a53d428ce87a2725dda5809d409
Mikael Ryynänen <mikael.ryynanen@live.com>
2021-06-11 11:04:11 +0300

    ll

--- public_html/index.php
+++ public_html/index.php
@@ -5,69 +5,98 @@ $result = mysqli_query($conn,$query);
 $row = mysqli_fetch_array($result); 
  ?>
 <html>
-	<link rel="stylesheet" href="styleshit.css">
-	<head>
-		<h1>hello world</h1>
-		<div class="box-wrapper">
-		<div <?php echo "id='box1'"?> style="<?php if ($row["varaus"] == 1) {echo 'background-color: blue;';} if ($row["varaus"] == 2) {echo 'background-color: red;';} ?>"> test</div>
-		</div> 
-		<form action="vara.php" method="get">
-		<select id="cars" name="cars">
+<link href="StyleSheet.css" rel="stylesheet">
+
+
+<title>Varaus</title>
+  
+<body>
+
+
+<div class="edgebox">
+
+	<div class="grid">
+	<p id = "title1">AITAT</p>
 		
-		<?php
-		//nämä tekee sen alas veto valikon
+		<div id = "box">
+			<div class="grid-container6">
+				<?php
+				$query = "SELECT * FROM `aitta` ORDER BY nimi ASC";
+				$result = mysqli_query($conn,$query);
+				$rowcount = 0;
+				while ($row = mysqli_fetch_array($result)) {
+				if ($rowcount==2) {echo "</div><div class='grid-container'>";}
+				if ($rowcount==12) {echo "</div><div class='grid-container2'>";}
+				if ($row["varaus"] == 1) 
+				{$mina = "background-color: #0CFF00;";} 
+				if ($row["varaus"] == 2) 
+				{$mina = "background-color: #F9FF31;";}
+				if ($row["varaus"] == 3) 
+				{$mina = "background-color: #FF3A00;";}
+				$rowcount++; 
+				echo "<div class='grid-item' style='".$mina."' ><h2>".$rowcount."</h2></div>";
+				
+				}
+				?>
+			</div>
+		</div>
+	</div>
+	
+	<div class="grid2">
+	<p id = "title2">PIENET TELTAT</p>	
+		<div id = "box">
+			<div class="grid-container3">
+				<?php 
+				$query = "SELECT * FROM `telttapaikatpieni` ORDER BY nimi ASC";
+				$result = mysqli_query($conn,$query);
+				$rowcount = 7;
+				while ($row = mysqli_fetch_array($result)) {
+				if ($row["varaus"] == 1) 
+				{$mina = "background-color: #0CFF00;";} 
+				if ($row["varaus"] == 2) 
+				{$mina = "background-color: #F9FF31;";}
+				if ($row["varaus"] == 3) 
+				{$mina = "background-color: #FF3A00;";}
+				$rowcount++; 
+				echo "<div class='grid-item' style='".$mina."' ><h2>".$rowcount."</h2></div>";}
+				?>
+			</div>
+		</div>
+	</div>
+	
+	<div class="grid3">
+		<p id = "title3">ISOT TELTAT</p>
+			<div id = "box">
+				<div class="grid-container5">
+						<?php 
+						$query = "SELECT * FROM `telttapaikatiso` ORDER BY nimi ASC";
+						$result = mysqli_query($conn,$query);
+						$rowcount = 0;
+						while ($row = mysqli_fetch_array($result)) {
+						if ($rowcount==1) {echo "</div><div class='grid-container4'>";}
+						if ($row["varaus"] == 1) 
+						{$mina = "background-color: #0CFF00;";} 
+						if ($row["varaus"] == 2) 
+						{$mina = "background-color: #F9FF31;";}
+						if ($row["varaus"] == 3) 
+						{$mina = "background-color: #FF3A00;";}
+						$rowcount++;
+						echo "<div class='grid-item' style='".$mina."' ><h2 id = 'custombox'>".$rowcount."</h2></div>";}
+						?>
+				
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+		//nÃ¤mÃ¤ tekee sen alas veto valikon
 	$query = "SELECT * FROM `nyt`";
 	$result = mysqli_query($conn,$query);
+		echo "<select>";
         while ($row = mysqli_fetch_array($result)) {
         echo "<option value='".$row["nimi"]."' >".$row["nimi"]."</option>";}
-		echo "<select><input type='submit' value='Run me now!'></input></form>"
+		echo "</select><input type='submit' value='Run me now!'></input></form>"
 	?>
-	<?php 
-	//korvaa grid item pieni teltta tällä
-	$query = "SELECT * FROM `telttapaikatpieni` ORDER BY nimi ASC";
-	$result = mysqli_query($conn,$query);
-        while ($row = mysqli_fetch_array($result)) {
-		if ($row["varaus"] == 1) 
-		{$mina = "background-color: green;";} 
-		if ($row["varaus"] == 2) 
-		{$mina = "background-color: yellow;";}
-		if ($row["varaus"] == 3) 
-		{$mina = "background-color: red;";}
-        echo "<div class='grid-item' style='".$mina."' ><h2>".$row."</h2></div>";}
-	?>
-	<?php 
-	//korvaa grid item aitat tällä
-	$query = "SELECT * FROM `aitta` ORDER BY nimi ASC";
-	$result = mysqli_query($conn,$query);
-	$rowcount = 0;
-
-        while ($row = mysqli_fetch_array($result)) {
-		if ($rowcount==2) {echo "</div><div class='grid-container'>"}
-		if ($rowcount==12) {echo "</div><div class='grid-container2'>"}
-		if ($row["varaus"] == 1) 
-		{$mina = "background-color: green;";} 
-		if ($row["varaus"] == 2) 
-		{$mina = "background-color: yellow;";}
-		if ($row["varaus"] == 3) 
-		{$mina = "background-color: red;";}
-        echo "<div class='grid-item' style='".$mina."' ><h2>".$row."</h2></div>";
-		$rowcount++
-		}
-	?>
-	<?php 
-	//korvaa grid item iso teltta tällä
-	$query = "SELECT * FROM `telttapaikatpieni` ORDER BY nimi ASC";
-	$result = mysqli_query($conn,$query);
-	$rowcount = 0;
-        while ($row = mysqli_fetch_array($result)) {
-		if ($rowcount==2) {echo "</div><div class='grid-container4'>"}
-		if ($row["varaus"] == 1) 
-		{$mina = "background-color: green;";} 
-		if ($row["varaus"] == 2) 
-		{$mina = "background-color: yellow;";}
-		if ($row["varaus"] == 3) 
-		{$mina = "background-color: red;";}
-        echo "<div class='grid-item' style='".$mina."' ><h2 id = 'custombox'>".$row."</h2></div>";}
-	?>
-	</head>
+</body>
+  
 </html>
